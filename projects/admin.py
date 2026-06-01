b44a1a4599f7b74a00cae1064b04d438603a0ad1
Coureau <julien.coureau@orange.fr>
2026-06-01 09:52:02 +0200

    ajout page admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Project, Contributor, Issue, Comment
 
-# Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "type", "author", "created_time")
+    list_filter = ("type",)
+    search_fields = ("name",)
+
+
+@admin.register(Contributor)
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "project", "created_time")
+    list_filter = ("project",)
+
+
+@admin.register(Issue)
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "project", "priority", "tag", "status", "author")
+    list_filter = ("priority", "tag", "status", "project")
+    search_fields = ("name",)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "issue", "author", "created_time")
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    """Affiche le modèle User custom dans l'admin Django avec les champs RGPD."""
+    list_display = (
+        "id", "username", "email", "birth_date",
+        "can_be_contacted", "can_data_be_shared", "is_staff",
+    )
+    list_filter = ("is_staff", "is_superuser", "can_be_contacted")
+    search_fields = ("username", "email")
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Informations RGPD", {
+            "fields": ("birth_date", "can_be_contacted", "can_data_be_shared")
+        }),
+    )
